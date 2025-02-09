ee1674913a228881591358a89f10593decaef4d3
i.kłusek <J6ZkqwVUn>
2025-02-09 21:10:27 +0100

    WN-01-Added project main layout structure && and global/vars css

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import AppMenu from '$lib/components/appMenu.svelte';
+	import '$lib/styles/global.scss';
+</script>
+
+<main>
+	<AppMenu />
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		outline: 1px solid red;
+		max-width: var(--max-width);
+		min-width: var(--min-width);
+		min-height: var(--min-height);
+		margin: auto;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+
+		position: relative;
+	}
+</style>
