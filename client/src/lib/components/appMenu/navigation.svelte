db1391ad1c3df395f0e39f710d4db6096a970b61
i.kłusek <J6ZkqwVUn>
2025-02-11 17:45:22 +0100

    WN-13-Added navigation UI

---
+++ client/src/lib/components/appMenu/navigation.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import ImgButton from '../ui/imgButton.svelte';
+	import SvgButton from '../ui/svgButton.svelte';
+</script>
+
+<div class="navigation-wrapper">
+	<div class="left-nav">
+		<SvgButton src="/svg/button/add.svg" alt="add" />
+	</div>
+	<div class="right-nav">
+		<SvgButton src="/svg/button/settings.svg" alt="settings" />
+		<ImgButton src="/img/profile.JPG" alt="profile" />
+		<SvgButton src="/svg/button/close.svg" alt="close" />
+	</div>
+</div>
+
+<style lang="scss">
+	.navigation-wrapper {
+		/* outline: 1px solid blue; */
+		width: 100%;
+		min-height: 55px;
+		gap: 1rem;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.left-nav {
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			gap: 1rem;
+		}
+
+		.right-nav {
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			gap: 1rem;
+		}
+	}
+</style>
