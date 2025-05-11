f61e3303c2f2c29b3d03352bba55f0987087b045
i.kłusek <J6ZkqwVUn>
2025-05-11 12:19:38 +0200

    WN-183-FIX - user authenticate after closing to browser

--- client/src/routes/(protected)/+layout.server.ts
+++ client/src/routes/(protected)/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	console.log('LayoutServerLoad: ', locals);
+	// console.log('LayoutServerLoad: ', locals);
 
 	return {
 		user: locals.user,
